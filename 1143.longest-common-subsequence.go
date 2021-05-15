928235f18d888d12665bf9f587e511802ed34111
Hyperliar <gghyperliar@gmail.com>
2021-05-15 23:53:18 +0800

    update: offer

---
+++ 1143.longest-common-subsequence.go
@@ -0,0 +1,12 @@
+/*
+ * @lc app=leetcode id=1143 lang=golang
+ *
+ * [1143] Longest Common Subsequence
+ */
+
+// @lc code=start
+func longestCommonSubsequence(text1 string, text2 string) int {
+    
+}
+// @lc code=end
+
--- 25.reverse-nodes-in-k-group.go
+++ 25.reverse-nodes-in-k-group.go
@@ -13,7 +13,60 @@
  * }
  */
 func reverseKGroup(head *ListNode, k int) *ListNode {
-    
+	if k == 1 {
+		return head
+	}
+	var temp, prev, lastTail, tail, newHead *ListNode
+	var i int
+	for head != nil {
+		i = 0
+		tail = head
+		for i < k && head != nil {
+			temp = head.Next
+			head.Next = prev
+			prev = head
+			head = temp
+			i++
+		}
+		if newHead == nil {
+			newHead = prev
+		}
+
+		if lastTail != nil {
+			lastTail.Next = prev
+		}
+		prev = nil
+
+		if i == k {
+			lastTail = tail
+		}
+	}
+
+	//print(newHead)
+	//fmt.Println(prev, tail, lastTail)
+	if i != k {
+		j := 0
+		head = lastTail.Next
+		for head != nil && j < i {
+			temp = head.Next
+			head.Next = prev
+			prev = head
+			head = temp
+			j++
+		}
+		lastTail.Next = prev
+	}
+
+	return newHead
 }
+
+func print(l *ListNode) {
+	for l != nil {
+		fmt.Print(l.Val)
+		l = l.Next
+	}
+	fmt.Println()
+}
+
 // @lc code=end
 
